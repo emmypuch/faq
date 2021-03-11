b4408008f322c0939944752812b4e90f81f405b7
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2021-03-11 13:18:28 +0100

    modified index.html and styles.css

---
+++ faq.js
@@ -0,0 +1,7 @@
+const accordion = document.querySelector(".contentBx");
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+        this.classList.toggle("active")
+    })
+}
