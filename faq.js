b5a6b3f6074d965ea3aeab5497fc33e43d3927ba
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2021-03-11 15:20:25 +0100

    updated

--- faq.js
+++ faq.js
@@ -1,6 +1,6 @@
-const accordion = document.querySelector(".contentBx");
+const accordion = document.querySelectorAll(".contentBx");
 
-for (i = 0; i < accordion.length; i++) {
+for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
         this.classList.toggle("active")
     })
